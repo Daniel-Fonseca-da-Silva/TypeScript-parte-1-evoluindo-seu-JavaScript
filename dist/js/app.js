0231c896792652bf302aa159e341838774f89c6f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-04-11 23:38:16 -0300

    Finalização do modelo

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,4 @@
-import { Negociacao } from "./models/negociacao.js";
+import { Negociacao } from "./models/negociacao.js"
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-negociacao.quantidade = 1000;
-console.log(negociacao);
+const negociacao = new Negociacao(new Date(), 10, 100)
+console.log(negociacao.volume)
