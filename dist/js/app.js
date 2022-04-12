474d13985d55c6191797d806dd0604bb19410373
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-04-11 22:31:49 -0300

    Negociação, modelagem e regras

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,6 @@
-alert('Hello!')
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao);
+negociacao.quantidade = 1000;
+console.log(negociacao);
