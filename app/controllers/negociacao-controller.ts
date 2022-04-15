bc3d59602e66788b534ee99c7ebcfa2dbd3aac58
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-04-15 20:30:49 -0300

    Ajustando nosso controller

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,8 +1,8 @@
 import { Negociacao } from './../models/negociacao.js';
 export class NegociacaoController {
-    private inputData
-    private inputQuantidade
-    private inputValor
+    private inputData: HTMLInputElement
+    private inputQuantidade: HTMLInputElement
+    private inputValor: HTMLInputElement
 
     constructor() {
         this.inputData = document.querySelector('#data')
