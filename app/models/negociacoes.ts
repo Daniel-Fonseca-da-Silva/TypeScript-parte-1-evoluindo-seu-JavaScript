375e12e161202a7090b7cbd5a0bbbf9c409896ec
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-04-17 13:33:20 -0300

    Um problema não esperado

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,4 +1,12 @@
 import { Negociacao } from './negociacao';
 export class Negociacoes{
     private negociacoes: Array<Negociacao> = []
+
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao)
+    }
+
+    lista(): Array<Negociacao> {
+        return  this.negociacoes
+    }
 }
