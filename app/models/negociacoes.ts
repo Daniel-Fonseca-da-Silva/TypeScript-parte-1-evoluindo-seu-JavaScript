d7a3bc787ffd04ef4eaf94d185d2d80f89edda78
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-04-17 21:51:24 -0300

    Arrays e Generics

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,6 +1,6 @@
 import { Negociacao } from './negociacao';
 export class Negociacoes{
-    private negociacoes: Array<Negociacao> = []
+    private negociacoes:Negociacao[] = []
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao)
